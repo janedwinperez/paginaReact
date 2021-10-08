39c3bacf2204f7400ed6412f5a1bacdb84003cef
janedwinperez <janedwin2000@gmail.com>
2021-10-08 18:22:06 -0500

    we place the Menu module

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Home from './pages/Home';
 import Productos from './pages/Productos';
 import Reportes from './pages/Reportes';
+import Menu from "./componentes/Menu";
+
 
 //const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
@@ -80,42 +82,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      {/*<img src={logo2}  alt="logo de nury" />*/}
-      <Router>
-      <div className="container">
-
-        <div className="btn-group">
-          <Link to="/" className="btn btn-dark">
-            Home
-          </Link>
-          <Link to="/productos" className="btn btn-dark">
-            Productos
-          </Link>
-          <Link to="/Reportes" className="btn btn-dark">
-            Reportes
-          </Link>
-      </div>
-      <hr/>
-
-      <div id="miSwitch">
-        <Switch>
-          <Route path='/' exact>
-            <Home/>
-          </Route>
-          <Route path='/productos'>
-            <Productos/>
-          </Route>
-          <Route path='/reportes'>
-            <Reportes/>
-          </Route>
-        </Switch>
-      </div>
-    </div>
-
-    </Router>
-
-
-
+        <Menu />
+        
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         
          {!isLoggedIn &&
